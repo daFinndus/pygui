9537e5ae34a12aac5bf85ddf3885a6d775355d69
Finn Luca Jensen <finnlucajensen555@gmail.com>
2023-12-07 12:32:42 +0100

    Updated gui.py

--- gui/gui_functions.py
+++ gui/gui_functions.py
@@ -1,16 +1,17 @@
 from gui import Ui_PyGUI
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Functions():
+class Ui_Functions:
     def __init__(self):
         self.ui = Ui_PyGUI()
-        self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.update_result)
+        self.ui.setupUi(Dialog)
+        self.ui.button_update.clicked.connect(self.update_result)
 
     def update_result(self):
-        temp_celsius = float(self.ui.lineEdit_celsius.text())
+        temp_celsius = float(self.ui.sampling_time_input.text())
         temp_kelvin = temp_celsius + 273.13
-        self.ui.lineEdit_celsius.setText(str(temp_kelvin) + " K")
+        self.ui.temperature_ouput.setText(str(temp_kelvin) + " K")
 
 
 if __name__ == "__main__":
